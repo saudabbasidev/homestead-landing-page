0014c43394776611d8668d9e945bde6850e3c5f0
saudabbasidev <saudabbasideveloper@gmail.com>
2026-07-06 23:21:08 +0500

    fixed navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@
 const Navbar = () => {
   return (
     <>
-        <nav className=" flex items-center gap-10 max-sm:px-2 p-10 justify-between ">
+        <nav className="max-w-400 flex items-center gap-10 max-sm:px-2 p-10 justify-between ">
         <a href="/">
           <img src="/logo.avif" alt="" className="max-h-7 object-contain" />
         </a>
